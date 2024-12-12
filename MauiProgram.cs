10e896d7802a19919df22c85b375304d2d114bf1
Nico Benninger <nico.benninger43@gmail.com>
2024-12-12 11:16:26 +0100

    Initial commit

---
+++ MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace PayBuddy;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("Inter.ttf", "Inter");
+                fonts.AddFont("Phosphor.ttf", "PhosphorIcons");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
